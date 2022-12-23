d1791b49d948f81e455104f275cb69549f7079bf
shaikfarzana2712 <105494774+shaikfarzana2712@users.noreply.github.com>
2022-12-23 17:49:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+noseX=0; noseY=0;
+function preload() {
+clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    postNet.on('pose', gotPoses);
+}
+    function gotPoses(result) {
+        console.log(result);
+        console.log("nosex=" + result[0].pose.nose.x)
+        console.log("nosey=" + result[0].pose.nose.y)
+noseX = result[0].pose.nose.x-15; noseY = result[0].pose.nose.y+10;
+    }
+
+    function draw() {
+        image(video, 0, 0, 300, 300);
+    image(clown_nose,noseX,noseY,30,30)
+}
+
+    function take_snapshot() {
+        save('myFilterImage.png')
+    }
+
+    function modelLoaded() {
+        console.log('PoseNet Is Initialized');
+    }
+    
